3c018b2c2aa59b69c2c82b79d7120a3a6b0db465
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-11-08 12:09:01 +0530

    Added carry flag register

--- impl/cpu.v
+++ impl/cpu.v
@@ -17,12 +17,14 @@ wire [0:16] w_aluB;
 wire [0:5]  w_aluOP;
 wire [0:16] w_aluOut;
 wire        w_aluCarry;
-
+wire        w_currCarry;
+wire        w_carryWriteCtrl;
 
 programrom rom(.i_addr(w_instrAddr), .o_data(w_progInstruction));
 memorystack stack(.bus(BUS), .i_addr(w_stkAddr), .i_w(w_stkW), .i_s(w_stkS), .o_top(w_aluB));
 stackpointer sp(.bus(BUS),.i_ctrl(w_spCtrl), .o_topAddr(w_spAddr));
 alu74181 alu(.i_A(w_aluA), i_B(w_aluB), .i_OP(w_aluOP), .o_out(w_aluOut), .o_carry(w_aluCarry));
 
+carryFlag bitreg(.i_data(w_aluCarry), .i_w(w_carryWriteCtrl), .o_data(w_currCarry));
 
 endmodule
