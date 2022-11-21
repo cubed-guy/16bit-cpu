89d95299a255f021e39f6612aeb0f536cb648aad
Samarth <samarthr1@outlook.com>
2022-11-21 22:56:58 +0530

    fix a syntax err

--- impl/tb.v
+++ impl/tb.v
@@ -3,7 +3,7 @@ reg clock;
 
 cpu c(.i_clock(clock));
 
-@always #10 clock = ~clock;
+always #10 clock = ~clock;
 
 initial
     $monitor("%d : %h %h %h %h", $time, c.stack.memory[0] , c.stack.memory[1], c.stack.memory[2], c.stack.memory[3]);
