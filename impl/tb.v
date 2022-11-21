e55839ed94955cfde94a3ffb1b9703f4b86e4b35
Samarth <samarthr1@outlook.com>
2022-11-21 22:13:28 +0530

    control unit

---
+++ impl/tb.v
@@ -0,0 +1,11 @@
+module tb();
+reg clock;
+
+cpu c(.i_clock(clock));
+
+@always #10 clock = ~clock;
+
+initial
+    $monitor("%d : %h %h %h %h", $time, c.stack.memory[0] , c.stack.memory[1], c.stack.memory[2], c.stack.memory[3]);
+
+endmodule
