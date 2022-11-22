8aeeaa593667c4e2304e75b39698c18a9befff1f
Samarth <samarthr1@outlook.com>
2022-11-22 11:40:42 +0530

    wip gimp alu

--- impl/increment.v
+++ impl/increment.v
@@ -13,6 +13,27 @@ module full_adder(input wire a, b, cin, output wire sum, cout);
   or O1(cout, c1, c2);
 endmodule
 
+module full_add16(input wire [15:0] i_a, input wire [15:0] i_b, input wire cin, output wire [15:0] o_out, output wire o_cout );
+wire Co[0:14];
+full_adder f0(.a(i_a[0]), .b(i_b[0]), .cin(cin), .sum(o_out[0]), .cout(Co[0]));
+full_adder f1(.a(i_a[1]), .b(i_b[1]), .cin(Co[0]), .sum(o_out[1]), .cout(Co[1]));
+full_adder f2(.a(i_a[2]), .b(i_b[2]), .cin(Co[1]), .sum(o_out[2]), .cout(Co[2]));
+full_adder f3(.a(i_a[3]), .b(i_b[3]), .cin(Co[2]), .sum(o_out[3]), .cout(Co[3]));
+full_adder f4(.a(i_a[4]), .b(i_b[4]), .cin(Co[3]), .sum(o_out[4]), .cout(Co[4]));
+full_adder f5(.a(i_a[5]), .b(i_b[5]), .cin(Co[4]), .sum(o_out[5]), .cout(Co[5]));
+full_adder f6(.a(i_a[6]), .b(i_b[6]), .cin(Co[5]), .sum(o_out[6]), .cout(Co[6]));
+full_adder f7(.a(i_a[7]), .b(i_b[7]), .cin(Co[6]), .sum(o_out[7]), .cout(Co[7]));
+full_adder f8(.a(i_a[8]), .b(i_b[8]), .cin(Co[7]), .sum(o_out[8]), .cout(Co[8]));
+full_adder f9(.a(i_a[9]), .b(i_b[9]), .cin(Co[8]), .sum(o_out[9]), .cout(Co[9]));
+full_adder f10(.a(i_a[10]), .b(i_b[10]), .cin(Co[9]), .sum(o_out[10]), .cout(Co[10]));
+full_adder f11(.a(i_a[11]), .b(i_b[11]), .cin(Co[10]), .sum(o_out[11]), .cout(Co[11]));
+full_adder f12(.a(i_a[12]), .b(i_b[12]), .cin(Co[11]), .sum(o_out[12]), .cout(Co[12]));
+full_adder f13(.a(i_a[13]), .b(i_b[13]), .cin(Co[12]), .sum(o_out[13]), .cout(Co[13]));
+full_adder f14(.a(i_a[14]), .b(i_b[14]), .cin(Co[13]), .sum(o_out[14]), .cout(Co[14]));
+full_adder f15(.a(i_a[15]), .b(i_b[15]), .cin(Co[14]), .sum(o_out[15]), .cout(o_cout));
+
+endmodule
+
 module incrementer(input [0:15] i_in, input i_dir, output [0:15] o_out);
   wire Co[0:15];
   wire w_dirInv;
