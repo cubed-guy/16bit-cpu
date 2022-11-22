26266f3eca94cdc2626e51242ab85d09b23ee379
Samarth <samarthr1@outlook.com>
2022-11-22 10:26:47 +0530

    initialise more registers

--- tb/tb_cpu.v
+++ tb/tb_cpu.v
@@ -8,6 +8,8 @@ initial begin
     clock = 1'b0;
     c.sp.top.o_data = 16'b0;
     c.ip.o_data     = 16'b0;
+    c.registerR.o_data = 16'b0;
+    c.registerT.o_data = 16'b0;
 end
 initial begin $dumpfile("cpu.vcd"); $dumpvars(0,tb); end
 initial begin
